52b2a3f16520f0e5cc73942b6d6382cba74a17bc
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-04-06 12:01:27 +0530

    ✨Add. Added the endpoints of the user service

---
+++ src/main/java/com/rapidshine/carwash/carwashapigateway/CarWashApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rapidshine.carwash.carwashapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarWashApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarWashApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rapidshine/carwash/carwashapigateway/CarWashApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidshine.carwash.carwashapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
